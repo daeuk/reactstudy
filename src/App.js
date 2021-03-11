c0833c46f848f3e5c51b63f0466e064286025b0e
daeuk <daeukleehan@gmail.com>
2021-03-11 22:57:33 +0900

    imac 11.mar.21 02

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="title">두잇! 리액트 시작하기</h1>
+        <h1 className="title">두잇! 리액트 시작하기!!</h1>
       </div>
     );
   }
